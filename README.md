92bb928e11ecb1a220adf3e333165cdf6ba8dfba
Guga Zimmermann <gugazimmermann@gmail.com>
2021-02-13 15:16:27 -0300

    flow

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # X-Team Go Bootcamp
 
-https://github.com/julianosouza/go-crash-course/tree/master/02-structs-and-functions
+https://github.com/julianosouza/go-crash-course/tree/master/04-flow-control
 
-## Pointers
+## Flow Control
 
-https://gobyexample.com/pointers
+https://gobyexample.com/for
+
+https://gobyexample.com/if-else
+
+https://gobyexample.com/switch
+
+https://gobyexample.com/switch
+
+https://gobyexample.com/slices
+
+https://gobyexample.com/maps
+
+https://gobyexample.com/range
--- main.go
+++ main.go
@@ -1,58 +1,5 @@
 package main
 
-import "fmt"
-
-func zeroval(ival int) {
-	ival = 0
-}
-
-func zeroptr(iptr *int) {
-	fmt.Println("iptr:", iptr)
-	fmt.Println("iptr dereference:", *iptr)
-	*iptr = 0
-}
-
-type char struct {
-	name string
-}
-
-func changeName1(c char) {
-	c.name = "A2"
-}
-
-func changeName2(c *char) {
-	c.name = "B2"
-}
-
-func changeName3(c *char) {
-	c.name = "C2"
-}
-
 func main() {
-	i := 1
-	fmt.Println("initial:", i)
-	zeroval(i)
-	fmt.Println("zeroval:", i)
-	fmt.Println("pointer:", &i)
-	zeroptr(&i)
-	fmt.Println("zeroptr:", i)
 
-	fmt.Println()
-	fmt.Println()
-
-	char1 := new(char)
-	char1.name = "A"
-	fmt.Println("Character name: ", char1.name)
-	changeName1(*char1)
-	fmt.Println("Character name: ", char1.name)
-
-	char2 := &char{"B"}
-	fmt.Println("Character name: ", char2.name)
-	changeName2(char2)
-	fmt.Println("Character name: ", char2.name)
-
-	char3 := char{"C"}
-	fmt.Println("Character name: ", char3.name)
-	changeName3(&char3)
-	fmt.Println("Character name: ", char3.name)
 }
