94a411452c3b8bd79288a1a3b33c3b5da6247f7c
Guga Zimmermann <gugazimmermann@gmail.com>
2021-02-14 17:02:28 -0300

    concurrency

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -47,6 +48,68 @@ func main() {
 	<-anyChan
 
 	fmt.Println()
+
+	ic := make(chan int)
+	go periodicSend(ic)
+	for i := range ic {
+		fmt.Println(i)
+	}
+	// ic <- 6
+	// panic: send on closed channel
+	// with this we can see if channel are open or close
+	_, ok := <-ic
+	fmt.Println("Channel are open?", ok)
+
+	fmt.Println()
+
+	// bufferd channels block when they are either full or empty
+
+	/* 	// Buffer 2, send 3 = deadlock
+	   	buffc := make(chan int, 2)
+	   	buffc <- 3 // FIFO
+	   	buffc <- 2
+	   	buffc <- 1
+	   	fmt.Println(<-buffc)
+	   	fmt.Println(<-buffc)
+	*/
+	/* 	// Buffer 5, have only 2, read 3 = deadlock
+	   	buffc := make(chan int, 5)
+	   	buffc <- 3 // FIFO
+	   	buffc <- 2
+	   	fmt.Println(<-buffc)
+	   	fmt.Println(<-buffc)
+	   	fmt.Println(<-buffc)
+	*/
+	buffc := make(chan int, 5)
+	buffc <- 3 // FIFO
+	buffc <- 2
+	fmt.Println(<-buffc)
+	fmt.Println(<-buffc)
+
+	fmt.Println()
+
+	// v2 return first, because wait less than v1
+
+	testCh := make(chan int)
+	select {
+	case v1 := <-waitAndSend(1, 5):
+		fmt.Println(v1)
+	case v2 := <-waitAndSend(5, 1):
+		fmt.Println(v2)
+	case testCh <- 20:
+		fmt.Println("testCh received a value")
+		// default:
+		// 	fmt.Println("All channels are slow")
+	}
+	// select can handle multiple channels, but testCh will
+	// not be invoked because do not have a goroutine listening.
+
+	// if default is not commented it will run first, because the
+	// fastest channel still has to wait 1 second.
+	// with default we can prevent select from blocking the code.
+
+	// if two or more channel return at the same time
+	// select will pick one at random.
 }
 
 func hello(wg *sync.WaitGroup) {
@@ -72,4 +135,24 @@ func sayHelloInOtherLanguage(ts chan string, lang, text string) {
 		translated = text
 	}
 	ts <- translated
+}
+
+func periodicSend(ic chan int) {
+	i := 1
+	for i <= 3 {
+		ic <- i
+		i++
+		time.Sleep(1 * time.Second)
+	}
+	// without close() = fatal error: all goroutines are asleep - deadlock!
+	close(ic)
+}
+
+func waitAndSend(v, t int) chan int {
+	retCh := make(chan int)
+	go func() {
+		time.Sleep(time.Duration(t) * time.Second)
+		retCh <- v
+	}()
+	return retCh
 }
